89f4bcac0257025ab60e9b3cecb5189c10fd714d
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-01-28 21:29:46 +0530

    added multiple file upload logic

--- code/remove_data_from_s3.py
+++ code/remove_data_from_s3.py
@@ -2,11 +2,11 @@ from utils import connect_to_aws_service, load_config
 import glob
 import os 
 
-def create_bucket(buck_name):
-    s3_client.delete_bucket(Bucket=buck_name)
+def delete_bucket(bucket_name):
+    s3_client.delete_bucket(Bucket=bucket_name)
    
-def push_to_s3_bucket(buck_name, key):
-    s3_client.create_object(Bucket=buck_name, Key=key)
+def delete_data_from_bucket(bucket_name, key):
+    s3_client.delete_object(Bucket=bucket_name, Key=key)
 
 if __name__ == '__main__':
 
@@ -16,17 +16,16 @@ if __name__ == '__main__':
     secret_key = config_data['aws_credentials']['secret_key']
 
     region_name = config_data['s3_bucket_details']['region_name']
-    buck_name = config_data['s3_bucket_details']['bucket_name']
+    bucket_name = config_data['s3_bucket_details']['bucket_name']
 
     folder_name = config_data['data']['folder_name']
     
     s3_client = connect_to_aws_service('s3', access_key, secret_key, region_name)
 
-    create_bucket(buck_name)
-    txt_files = glob.glob('../' + folder_name + '/*.txt')
+    txt_files = glob.glob('../data/*.txt')
 
     for filename in txt_files:
         key = "%s/%s" % (folder_name, os.path.basename(filename))
-        push_to_s3_bucket('../' + key ,buck_name, key)
+        delete_data_from_bucket(bucket_name, key)
  
-    
+    delete_bucket(bucket_name)
--- code/utils.py
+++ code/utils.py
@@ -2,27 +2,18 @@ import boto3
 import yaml
 from yaml.loader import SafeLoader
 
-# Open the file and load the file
 def load_config():
-    with open('C:/Users/Tanvi Wagh/Documents/odmp/Movies_Analysis/config/config.yml') as f:
-        config_data = yaml.load(f, Loader=SafeLoader)
+    with open('../config/config.yml') as file:
+        config_data = yaml.load(file, Loader=SafeLoader)
+
     return config_data
 
-    
-# ACCESS_KEY = data['aws_credentials']['ACCESS_KEY']
-# SECRET_KEY = data['aws_credentials']['SECRET_KEY']
-
-def connect_to_aws_service(service_name,ACCESS_KEY,SECRET_KEY):
+def connect_to_aws_service(service_name, access_key, secret_key, region_name):
     aws_client = boto3.client(service_name, 
-                  region_name='us-east-1', 
-                  aws_access_key_id=ACCESS_KEY, 
-                  aws_secret_access_key=SECRET_KEY)
+                 region_name=region_name,
+                 aws_access_key_id=access_key, 
+                 aws_secret_access_key=secret_key)
+
     return aws_client
-
-'''
-def connect_to_aws_service(service_name)
-
-
-'''
 
 
