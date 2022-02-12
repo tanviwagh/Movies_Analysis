0fa005589a8347113a452604f42a74791c13674a
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-02-12 19:53:50 +0530

    rough code v2

--- code/data_cleaning.py
+++ code/data_cleaning.py
@@ -1,6 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, regexp_extract, regexp_replace, explode
+from pyspark.sql.functions import col, regexp_extract, regexp_replace, explode, split
 
 PATH = ""
 
@@ -14,6 +14,8 @@ df1 = df.select(regexp_extract('original air date', r'\([^)]*\)', 0).alias('orig
 
 df2 = df.select(regexp_replace('original air date', r'\([^)]*\)', "").alias('original_air_date'))
 
+cols_list = ['cast','music department','directors', 'writers', 'producers']
+
 def remove_duplicates(dataframe):
     unique_dataframe = dataframe.dropDuplicates()
     return unique_dataframe
@@ -24,3 +26,23 @@ def save_to_parquet(dataframe):
 def explode_array_columns(dataframe, col_name):
     exploded_dataframe = dataframe.select(df['imdbID'],explode(df[col_name]))
     return exploded_dataframe
+
+def convert_to_array_type(df, col):
+    alias_name = col + '_alias'
+    df = df.select(df['*'], split(col(col),",").alias(alias_name))
+    return df
+
+
+def get_dataframes(df):
+    for col in cols_list:
+        d_type = dict(df.dtypes)[col]
+    if d_type == 'array<string>':
+        df1 = explode_array_columns(df, col)   
+    else:
+        df1 = df.select(col)
+    df1.show()
+
+for col in cols_list:
+    d_type = dict(df.dtypes)[col]
+    if d_type == 'string':
+        df = convert_to_array_type(df, col)
