f0545f9296994e7b9a72c4f87f804b9f8988a8ef
tanviwagh <tanvi.wagh26@gmail.com>
2022-02-20 20:25:30 +0530

    EMR test draft1

--- code/data_cleaning.py
+++ code/data_cleaning.py
@@ -6,13 +6,21 @@ APP_NAME = "data_cleaning"
 
 
 def read_json(folder_name, dir):
-    dataframe = spark.read.option("multiline","true").json("../" + folder_name + "/" + dir + "/*.json")
+    #dataframe = spark.read.option("multiline","true").json("../" + folder_name + "/" + dir + "/*.json")
+    dataframe = spark.read.option("multiline","true").json("file:////home/hadoop/Movies_Analysis/" + folder_name + "/" + dir + "/*.json")
     
+    cols = [col('imdbID'), col('localized title'), col('languages'), col('runtimes'), col('original air date'),
+         col('plot'), col('cast'), col('music department'), col('genres'),
+       col('directors'), col('writers'), col('producers')]
+
+    dataframe = dataframe.select(*cols)
+    dataframe = dataframe.withColumnRenamed("localized title", "localized_title")
+    dataframe = dataframe.withColumnRenamed("music department", "music_department")
     return dataframe 
 
 
 def clean_date_column(dataframe):
-    cols = [col('imdbID'), col('localized title'), col('languages'), col('runtimes'), col('original_air_date'),
+    cols = [col('imdbID'), col('localized_title'), col('languages'), col('runtimes'), col('original_air_date'),
         col('original_air_date_country'), col('plot')]
 
     temp_dataframe = dataframe.withColumn("original_air_date_country", regexp_extract('original air date', r'\([^)]*\)', 0))
@@ -25,17 +33,20 @@ def clean_date_column(dataframe):
 
 
 def convert_list_column(dataframe):
-    join_udf = udf(lambda x: ",".join(x))
-    join_dataframe = dataframe.withColumn("plot", join_udf(col("plot")))
+    if dict(dataframe.dtypes)["plot"] != "string":
+        join_udf = udf(lambda x: ",".join(x))
+        join_dataframe = dataframe.withColumn("plot", join_udf(col("plot")))
+        final_dataframe = join_dataframe.withColumn("plot", regexp_replace('plot', r'\]|\[', ""))
 
-    split_udf = udf(lambda x: x.split(',')[0])
-    split_dataframe = join_dataframe.withColumn("languages", split_udf(col("languages")))
+    else:
+        join_dataframe = dataframe
+        split_udf = udf(lambda x: x.split(',')[0])
+        split_dataframe = join_dataframe.withColumn("languages", split_udf(col("languages")))
 
-    result_dataframe = split_dataframe.withColumn("plot", regexp_replace('plot', r'\]|\[', ""))
-
-    final_dataframe = result_dataframe.withColumn("languages", regexp_replace('languages', r'\]|\[', ""))
+        final_dataframe = split_dataframe.withColumn("languages", regexp_replace('languages', r'\]|\[', ""))
 
     return final_dataframe
+
 
 
 def remove_duplicates(dataframe):
@@ -50,7 +61,7 @@ def save_to_parquet(dataframe, parquet_path):
 
 def convert_to_array_type(dataframe, col_name):
     array_dataframe = dataframe.withColumn(col_name, split(dataframe[col_name],",")).\
-                      select(col('imdbID'), col('cast'), col('music department'), 
+                      select(col('imdbID'), col('cast'), col('music_department'), 
                                 col('genres'), col('directors'), col('writers'), 
                                 col('producers')).withColumnRenamed('col', col_name)
     
@@ -59,7 +70,7 @@ def convert_to_array_type(dataframe, col_name):
 
 def explode_array_columns(dataframe, col_name):
     exploded_dataframe = dataframe.withColumn(col_name, explode(dataframe[col_name])).\
-                         select(col('imdbID'), col('cast'), col('music department'), 
+                         select(col('imdbID'), col('cast'), col('music_department'), 
                                 col('genres'), col('directors'), col('writers'), 
                                 col('producers')).withColumnRenamed('col', col_name)
 
@@ -88,16 +99,18 @@ if __name__=="__main__":
 
         formatted_df = convert_list_column(formatted_df)
 
-        output_path = '../' + parquet_folder_name + '/' + dir + '/' + "movie"
+        output_path = "file:////home/hadoop/Movies_Analysis/" + parquet_folder_name + '/' + dir + '/' + "movie"
 
         if not os.path.exists(output_path):
-                os.makedirs(output_path)
-
+            os.makedirs(output_path)
+        else:
+            print("Path exits")
+        
         save_to_parquet(formatted_df, output_path)
             
-        base_df = formatted_df
+        base_df = input_df
 
-        cols_list = ['cast','music department', 'genres','directors', 'writers', 'producers']
+        cols_list = ['cast','music_department', 'genres','directors', 'writers', 'producers']
 
         for col_name in cols_list:
             d_type = dict(base_df.dtypes)[col_name]
@@ -128,7 +141,8 @@ if __name__=="__main__":
         unique_producer_df = remove_duplicates(producer_df)
 
         for col_name in cols_list:
-            output_path = '../' + parquet_folder_name + '/' + dir + '/' + col_name
+            #output_path = '../' + parquet_folder_name + '/' + dir + '/' + col_name
+            output_path = "file:////home/hadoop/Movies_Analysis/" + parquet_folder_name + '/' + dir + '/' + col_name
 
             if not os.path.exists(output_path):
                 os.makedirs(output_path)
