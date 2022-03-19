238c649829e11a244beeda8f98f14cca277107b9
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-03-19 18:54:52 +0530

    few changes in DAG

--- dags/movie_data_dag.py
+++ dags/movie_data_dag.py
@@ -1,16 +1,3 @@
-'''
-----------------------------------------------
-Project: Movie Analysis
-File: movie_data_dag.py
-Description:
-    
-    ---DAG which runs from Tuesday to Saturday
-    at 8 AM IST in order to extracts data from
-    NSE website---
-    
------------------------------------------------
-'''
-
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
@@ -19,11 +6,39 @@ from airflow.utils.trigger_rule import TriggerRule
 from airflow import DAG, AirflowException
 import subprocess
 import json
+import boto3
+import yaml
+from yaml.loader import SafeLoader
 
 START_DATE = datetime.now() - timedelta(minutes=1470)
 
-# with open('config.json') as file:
-#   config_data = json.load(file)
+CONFIG_PATH = "s3://movie-analysis-code-bucket/app/conf/config.yml"
+
+EMR_IP_ADDRESS = ""
+
+SSH_CONNECT = "ssh -i /usr/local/airflow/ssh/emr-kpx.pem hadoop@"
+
+def path_to_bucket_key(path):
+    path = path.split('/')
+    bucket = path[2]
+    key = '/'.join(path[3:])
+
+    return bucket, key
+
+
+def read_s3_file(path, encoding='utf8'):
+    s3_client = boto3.client('s3')
+
+    bucket, key = path_to_bucket_key(path)
+
+    obj = s3_client.get_object(Bucket=bucket, Key=key)
+
+    return obj['Body'].read().decode(encoding)
+
+
+file = read_s3_file(CONFIG_PATH)
+
+config = yaml.load(file, Loader=SafeLoader)
 
 get_movie_data_script_path = config['dags']['get_movie_data_script_path']
 
@@ -33,39 +48,13 @@ schema_creation_script_path = config['dags']['schema_creation_script_path']
 
 data_quality_check_script_path = config['dags']['data_quality_check_script_path']
 
-config_path = config['dags']['config_path']
+execute_get_movie_data_script_cmd = f"{SSH_CONNECT} {EMR_IP_ADDRESS} spark-submit main.py '{'job_name': get_movie_data_script_path, 'path':{CONFIG_PATH}}'"
 
-# execute_holiday_script_cmd = f"python {holiday_script_path}"
+execute_data_cleaning_script_cmd = f"{SSH_CONNECT} {EMR_IP_ADDRESS} spark-submit main.py '{'job_name': data_cleaning_script_path, 'path':{CONFIG_PATH}}'"
 
-execute_get_movie_data_script_cmd = f"spark-submit main.py '{'job_name': get_movie_data_script_path, 'path':{config_path}}'"
+execute_schema_creation_script_cmd = f"{SSH_CONNECT} {EMR_IP_ADDRESS} spark-submit main.py '{'job_name': schema_creation_script_path, 'path':{CONFIG_PATH}}'"
 
-execute_data_cleaning_script_cmd = f"spark-submit main.py '{'job_name': data_cleaning_script_path, 'path':{config_path}}'"
-
-execute_schema_creation_script_cmd = f"spark-submit main.py '{'job_name': schema_creation_script_path, 'path':{config_path}}'"
-
-execute_data_quality_check_script_cmd = f"spark-submit main.py '{'job_name': data_quality_check_script_path, 'path':{config_path}}'"
-
-
-# def validate_day():
-#   output = subprocess.call(execute_holiday_script_cmd)
-
-#   if output != 0:
-#     print("Invalid day to run DAG")
-#     return "invalid_day"
-
-#   else:
-#     print("current_data script is scheduled")
-#     return "run_script"
-
-
-def data_quality_check():
-   output = subprocess.call(execute_data_quality_check_script_cmd)
-
-   if output != 0:
-     raise Exception("Data Quality Check has failed")
-
-   else:
-     print("Data Quality Check has been successful")
+execute_data_quality_check_script_cmd = f"{SSH_CONNECT} {EMR_IP_ADDRESS} spark-submit main.py '{'job_name': data_quality_check_script_path, 'path':{CONFIG_PATH}}'"
 
     
 default_args = {
@@ -86,9 +75,6 @@ dag_obj = DAG('get_daily_data_dag', max_active_runs=1, schedule_interval="0 3 * 
 
 start_task = DummyOperator(task_id = "start", dag = dag_obj)
 
-# validate_day_task = BranchPythonOperator(task_id="validate_day", python_callable = validate_day, dag = dag_obj)
-
-# run_script = BashOperator(task_id="run_script", bash_command = execute_current_script_cmd, dag = dag_obj)
 get_movie_data_task = BashOperator(task_id="get_movie_data", bash_command = execute_get_movie_data_script_cmd, dag = dag_obj)
 
 data_cleaning_task = BashOperator(task_id="data_cleaning", bash_command = execute_data_cleaning_script_cmd, dag = dag_obj)
@@ -96,10 +82,6 @@ data_cleaning_task = BashOperator(task_id="data_cleaning", bash_command = execut
 schema_creation_task = BashOperator(task_id="schema_creation", bash_command = execute_schema_creation_script_cmd, dag = dag_obj)
 
 data_quality_check_task = BashOperator(task_id="data_quality_check", bash_command = execute_data_quality_check_script_cmd, dag = dag_obj)
-
-dq_check = BashOperator(task_id="data_quality_check", python_callable=data_quality_check, dag=dag_obj)
-
-# invalid_day = DummyOperator(task_id="invalid_day", dag=dag_obj)
 
 end_task = DummyOperator(task_id="end", dag=dag_obj, trigger_rule=TriggerRule.ONE_SUCCESS)
 
