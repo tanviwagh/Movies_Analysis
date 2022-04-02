2f634a8ff15fd5c2c904b1bc971a81454901316d
nehaprabhavalkar <nehaprabhavalkar47@gmail.com>
2022-04-02 18:26:12 +0530

    commit after testing sentiment analysis

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from app.run import run
 import sys 
 import ast 
 
-# spark-submit main.py "{'job_name': 'get_movie_data', 'path':'s3://movie-analysis-code-bucket/app/conf/config.yml'}"
+# spark-submit main.py "{'job_name': 'get_movie_data', 'path':'s3://movie-analysis-code-bucket/Movie_Analysis/app/conf/config.yml'}"
 
-# spark-submit main.py "{'job_name': 'schema_creation', 'path':'s3://movie-analysis-code-bucket/app/conf/config.yml'}"
+# spark-submit main.py "{'job_name': 'schema_creation', 'path':'s3://movie-analysis-code-bucket/Movie_Analysis/app/conf/config.yml'}"
 
 if __name__ == "__main__":
     str_params = sys.argv[1]
